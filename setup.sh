83cef4974cda4a29ceb4235ae7b0f14a92237773
Sagar Rao <ksheerasagar17@users.noreply.github.com>
2023-02-07 23:49:58 -0600

    Adding download link for Miniconda

--- setup.sh
+++ setup.sh
@@ -1,14 +1,23 @@
-# this line runs the Miniconda3 installation script for MacOS, which installs Miniconda3 on the user's machine.
+# Download Miniconda or Python
+# For Miniconda you can go to below URL.
+https://docs.conda.io/en/latest/miniconda.html
+
+# Runs the Miniconda3 installation script for MacOS, which installs Miniconda3 on the user's machine.
 bash Miniconda3-latest-MacOSX-x86_64.sh
-# this line creates a new virtual environment named "streamlit" and sets the python version to 3.10.8. 
+
+# Creates a new virtual environment named "streamlit" and sets the python version to 3.10.8. 
 # The "-y" option means it will run without confirming and "-n" option is used to specify the name of the environment.
 conda create -y -n streamlit python=3.10.8
-# this line activates the "streamlit" environment, so all packages installed after this point will be installed within the "streamlit" environment.
+
+# Activates the "streamlit" environment, so all packages installed after this point will be installed within the "streamlit" environment.
 conda activate streamlit
-# this line installs the streamlit package, which is a web framework for building data apps in python.
+
+# Installs the streamlit package, which is a web framework for building data apps in python.
 pip install streamlit
-# this line exports the OPENAPI_KEY environment variable, which is used in the python script to access the OpenAI API.
+
+# Exports the OPENAPI_KEY environment variable, which is used in the python script to access the OpenAI API. This is fake key. Don't use it.
 export OPENAPI_KEY=sk-9wvblVeu2fMYmgo7t1gqjkcjAvW0uOaTNQ3lG6HKt37PjSUa
-# this line runs the generate_smart_business_names.py script using the streamlit run command. 
+
+# Generate_smart_business_names.py script using the streamlit run command. 
 # The script will run within the "streamlit" environment and have access to the streamlit package and OPENAPI_KEY environment variable.
 streamlit run generate_smart_business_names.py
