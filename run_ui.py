1b8ba5b08280c5f4a64379a9f5708540f9234049
Sagar Rao <ksheerasagar17@gmail.com>
2023-02-15 22:11:38 -0600

    add chat and business name generator in single website

--- run_ui.py
+++ run_ui.py
@@ -6,6 +6,7 @@ import os
 # URL for OpenAI API endpoint for completions
 url = "https://api.openai.com/v1/completions"
 
+
 def generate_name(business_domain_industry):
     prompt = f"Please generate 10 business names with explanations for a company that is related to {business_domain_industry} industry. Make sure the business names are short, unique, and catchy, and the explanations provide insight into the thought process behind each name."
 
@@ -31,16 +32,53 @@ def generate_name(business_domain_industry):
 
     return response.json()['choices'][0]['text']
 
+
+def prompt_name(prompt_text):
+    payload = {
+        "model": "text-davinci-003",
+        "prompt": prompt_text,
+        "temperature": 0.9,
+        "max_tokens": 500,
+        "top_p": 1,
+        "frequency_penalty": 0,
+        "presence_penalty": 0.6
+    }
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {os.getenv("OPENAPI_KEY")}'
+    }
+
+    response = requests.post(url, headers=headers, json=payload)
+
+    if not 200 <= response.status_code < 300:
+        st.exception(f"HTTP error {response.status_code}. {response.text}")
+
+    return response.json()['choices'][0]['text']
+
+
 def main():
-    st.title("Smart Business Name Generator")
+    st.title("Open AI GPT-3 Prompter")
 
-    search_term = st.text_input("Enter your business industry")
+    genre = st.radio(
+        "What do you want to use GPT-3 for?",
+        ('Business Name Generation', 'Chat'))
 
-    if search_term:
-        with st.spinner('Generating names...'):
-            results = generate_name(search_term)
-            st.write(results)
-            st.success("Done!")
+    search_term = st.text_input("Enter your selection here")
+
+    if genre == 'Business Name Gen':
+        if search_term:
+            with st.spinner('Generating names...'):
+                results = generate_name(search_term)
+                st.write(results)
+                st.success("Done!")
+    else:
+        if search_term:
+            with st.spinner('Getting chat response...'):
+                results = prompt_name(search_term)
+                st.write(results)
+                st.success("Done!")
+
 
 if __name__ == '__main__':
     main()
